cb997e2f10867726e86d77d3d2fd47e5d60db33f
3SESWATINI <santoshsingh20048@gmail.com>
2022-04-13 15:42:10 +0200

    Added a calculate file to the solution

---
+++ OurFirstApplicaton/calculate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurFirstApplicaton
+{
+    public  class calculate
+    {
+        public void docalculate()
+        {
+            int x = 0;
+            int y = 0;
+            int sum = 0;
+            sum = x + y;
+            Console.WriteLine("" + sum);
+        }
+    }
+}
