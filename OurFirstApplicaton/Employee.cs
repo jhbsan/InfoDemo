d4d962337e22fb9b55e6ca4a19a2a84eb4497a5c
3SESWATINI <santoshsingh20048@gmail.com>
2022-04-18 14:28:41 +0200

    First Trigger to teamcity for first change in the application

---
+++ OurFirstApplicaton/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurFirstApplicaton
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string  EmpName { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
